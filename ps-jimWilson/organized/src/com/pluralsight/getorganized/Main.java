29f1ff6485f2249b701f12556b73983a475fa95f
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-17 12:23:44 -0700

    organized by package

---
+++ ps-jimWilson/organized/src/com/pluralsight/getorganized/Main.java
@@ -0,0 +1,9 @@
+package com.pluralsight.getorganized;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Success Organized :)");
+    }
+}
