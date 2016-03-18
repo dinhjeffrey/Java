29149159a5fe66d57ef8f2a4fd7112ed71aee534
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-17 17:28:10 -0700

    added this keyword

--- ps-jimWilson/flight/src/com/pluralsight/flight/Main.java
+++ ps-jimWilson/flight/src/com/pluralsight/flight/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Flight lax1 = new Flight();
         Flight lax2 = new Flight();
-        Flight lax3;
+        Flight lax3 = null; // null is a reference literal. It represents an uncreated object
 
         for (int i=0; i<148; i++){
             lax1.add1Passenger();
@@ -18,8 +18,13 @@ public class Main {
             System.out.println("seats: " + lax3.seats);
             System.out.println("passengers: " + lax3.passengers);
         } else {
-            System.out.println("Doesn't have room");
+            System.out.println("Doesn't have room. lax1 + lax2 passengers is greater than lax1 seats (150)");
         }
+
+        if (lax3 != null)
+            System.out.println("Flight combined!");
+        else
+            System.out.println("Flights not combined.");
 
 
 
@@ -54,8 +59,9 @@ public class Main {
 
         public boolean hasRoom(Flight f2) {
             // first passengers is passengers from lax1 if called like this lax1.hasRoom(lax2)
-            int total = passengers + f2.passengers;
-            return total <= seats;
+            // 'this' keyword is an implicit reference to the current object lax1
+            int total = this.passengers + f2.passengers;
+            return total <= this.seats;
         }
 
         public Flight createNewWithBoth (Flight f2) {
